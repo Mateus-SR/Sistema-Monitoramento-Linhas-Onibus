5b384ca198a1f24d194dc3406ebdf3ad41f4410a
Mateus Santos Rodrigues <hiroshi.reserva@gmail.com>
2025-09-08 13:09:07 -0300

    Nova exibição (mandou bem, Pedro)
    + ajustes nos arquivos

--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './*.{html,js}',       // Escaneia arquivos na pasta raiz
-    './src/**/*.{html,js}' // Escaneia arquivos dentro da pasta src
-  ],
-}
