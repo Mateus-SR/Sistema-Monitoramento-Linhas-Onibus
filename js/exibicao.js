aef5adf7286aa91a4c23534dd37851dd7f45dfa5
Mateus Santos Rodrigues <hiroshi.reserva@gmail.com>
2025-09-13 13:59:30 -0300

    Botão de zoom

--- js/exibicao.js
+++ js/exibicao.js
@@ -1,14 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    const htmlElement = document.documentElement;
+
     const HideBtn = document.getElementById("hideBtn1");
     const HideElement = document.getElementById("hideElement1");
     const Clock = document.getElementById("horas");
+    const minus = document.getElementById('minus');
+    const plus = document.getElementById('plus');
+    const minusPlus = document.getElementById('minusPlus');
+
 
     const HideBtnText = document.getElementById('HideBtnText');
     let isHidden = false;
 
     HideBtn.addEventListener('click', () => {
         HideElement.classList.toggle('-translate-y-full');
+        // minusPlus.classList.toggle('-translate-x-full');
 
         isHidden = HideElement.classList.contains('-translate-y-full');
 
@@ -45,4 +52,36 @@ document.addEventListener('DOMContentLoaded', () => {
         };
         return variavel;
     }
+
+
+    function mudarZoom() {
+        const zoomValor = zoomNivel[zoomAtual];
+        htmlElement.style.fontSize = `${zoomValor}%`;
+
+        if (zoomAtual === 0) {
+            minus.classList.toggle('hover:text-sptrans');
+        } else if (zoomAtual === zoomNivel.length - 1) {
+            plus.classList.toggle('hover:text-sptrans');
+        } else {
+            minus.classList.add('hover:text-sptrans');
+            plus.classList.add('hover:text-sptrans');
+        }
+    };
+
+    const zoomNivel = [50, 67, 75, 90, 100, 110, 125, 133, 150];
+    let zoomAtual = 4;              // ^ esse aqui
+
+    plus.addEventListener('click', () => {
+            if (zoomAtual < zoomNivel.length - 1) {
+                zoomAtual++;
+                mudarZoom();
+            };
+        }),
+
+        minus.addEventListener('click', () => {
+            if (zoomAtual > 0) {
+                zoomAtual--;
+                mudarZoom();
+            };
+        })
 });
