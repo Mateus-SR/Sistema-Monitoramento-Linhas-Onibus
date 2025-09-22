2aca887e53d74e58756c2c480d971f925f034dfa
Mateus Santos Rodrigues <hiroshi.reserva@gmail.com>
2025-09-22 00:30:28 -0300

    Testes Express+Vercel+Axios de novo

--- server.js
+++ server.js
@@ -10,23 +10,46 @@ async function tokenPOST() {
     try {
       console.log('Enviando POST');
       // ATENÇÃO: Substitua pela URL de autenticação da sua API
-      const response = await axios.post(apiURL, {
-        tolkien
-      });
+      const response = await axios.post(`${apiURL}/Login/Autenticar?token=${tolkien}`);
   
-      accessToken = response.data.token; 
-      console.log('Sucesso!');
-
+      // A resposta da SPTrans em caso de sucesso é o booleano 'true' e um cookie.
+      // O status da resposta será 200.
+      if (response.data === true) {
+        console.log(`Sucesso na autenticação! Status: ${response.status}`);
+        // Esse ainda é um caso apenas de teste, e não estamos guardando nenhuma informação importante para rodar as proximas consultas na api
+        return { success: true, status: response.status, data: response.data };
+      } else {
+        // Se a resposta não for 'true', algo deu errado mesmo com status 200.
+        throw new Error('Autenticação falhou, resposta inesperada.');
+      }
+  
     } catch (error) {
-      console.error('Erro! ', error.message);
+        console.log(`Erro na autenticação!`);
+        if (error.response) {
+          // Se o erro veio da API (ex: token inválido)
+          console.error(`Status: ${error.response.status}`);
+          console.error(`Data: ${error.response.data}`);
+          return { success: false, status: error.response.status, data: error.response.data };
+        } else {
+          // Erro de rede ou outro problema
+          console.error(`Mensagem: ${error.message}`);
+          return { success: false, message: error.message };
+        }
     }
   }
 
 
 // Definições das rotas (o que fazer quando chamarmos tal coisa)
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello from Express on Vercel!' });
+  res.json({ message: 'Alo olá! Servidor rodando.' });
 });
+
+app.get('/testar-auth', async (req, res) => {
+    console.log('Rota de teste /testar-auth foi chamada!');
+    const resultado = await tokenPOST(); // Executa a função de verificação (ali de cima)
+    // Envia o resultado da autenticação de volta para o navegador
+    res.json(resultado);
+  });
  
 // Linha que faz o Vercel cuidar de executar tudo
 module.exports = app;
