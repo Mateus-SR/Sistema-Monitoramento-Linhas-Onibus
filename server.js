0d50ae7177a9712fa5468e8415826a1d48ca831f
Mateus Santos Rodrigues <hiroshi.reserva@gmail.com>
2025-09-21 23:13:04 -0300

    Preparando node.js + express.js + vercel
    Autoexplicativo

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+app.get('/home', (req, res) => {
+  res.status(200).json('Welcome, your app is working well');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
+
+module.exports = app;
