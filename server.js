f09c98da3ce09563653d29b3f804685b42e252ce
Mateus Santos Rodrigues <hiroshi.reserva@gmail.com>
2025-09-21 23:46:51 -0300

    Ajuste minimo server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 // Use "type: commonjs" in package.json to use CommonJS modules
 const express = require('express');
 const app = express();
- 
+
+const tolkien = process.env.tolkien;
+
 // Define your routes
 app.get('/', (req, res) => {
   res.json({ message: 'Hello from Express on Vercel!' });
