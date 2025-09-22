6e32fdb44c18470af819462a0f838627f9a706ae
Mateus Santos Rodrigues <hiroshi.reserva@gmail.com>
2025-09-22 00:12:04 -0300

    Teste funcionamento Express+Axios+Vercel

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
-// Use "type: commonjs" in package.json to use CommonJS modules
-const express = require('express');
+// Use "type: commonjs" in package.json to use CommonJS modules (comentario do exemplo do Vercel)
+const express = require('express'); // Para facilitar o uso geral do node.js
+const axios = require('axios'); // Para facilicar o uso de fetchs (chamar os dados da api)
 const app = express();
 
 const tolkien = process.env.tolkien;
+const apiURL = 'https://api.olhovivo.sptrans.com.br/v2.1'
 
-// Define your routes
+async function tokenPOST() {
+    try {
+      console.log('Enviando POST');
+      // ATENÇÃO: Substitua pela URL de autenticação da sua API
+      const response = await axios.post(apiURL, {
+        tolkien
+      });
+  
+      accessToken = response.data.token; 
+      console.log('Sucesso!');
+
+    } catch (error) {
+      console.error('Erro! ', error.message);
+    }
+  }
+
+
+// Definições das rotas (o que fazer quando chamarmos tal coisa)
 app.get('/', (req, res) => {
   res.json({ message: 'Hello from Express on Vercel!' });
 });
  
-// Export the Express app
+// Linha que faz o Vercel cuidar de executar tudo
 module.exports = app;
