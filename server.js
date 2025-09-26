a781e5519cd2d60a7fc0cd7cfe637025f328f42a
Mateus Santos Rodrigues <hiroshi.reserva@gmail.com>
2025-09-26 00:16:43 -0300

    Acabei esquecendo algo no ultimo commit

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.get('/parada-radar', async (req, res) => {
       ponto: resultadoPesquisa2.p.cp,
 
       linhas: resultadoPesquisa2.p.l.map(linhaIndividual => {
-        let veiculoExistente = null;
+        let proximoOnibus = null;
         if (linhaIndividual.vs && linhaIndividual.vs.length > 0) {
           proximoOnibus = {
             proximoOnibusCodigo: linhaIndividual.vs[0].p,
@@ -116,6 +116,7 @@ app.get('/parada-radar', async (req, res) => {
           codigoLetreiro: linhaIndividual.c,
           sentidoLinha: linhaIndividual.sl === 1 ? linhaIndividual.lt0 : linhaIndividual.lt1,
           quantidadeOnibus: linhaIndividual.qv,
+          proximoOnibus: proximoOnibus
         };
       })
     };
